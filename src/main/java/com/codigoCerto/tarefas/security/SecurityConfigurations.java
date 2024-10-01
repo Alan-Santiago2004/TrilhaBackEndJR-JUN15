9a27c1f140f372c0ce7f23b1e682a590a535ec72
Alan Santiago <alanorsantiago10@gmail.com>
2024-10-01 13:19:53 -0300

    feat: update SecurityConfigurations

--- src/main/java/com/codigoCerto/tarefas/security/SecurityConfigurations.java
+++ src/main/java/com/codigoCerto/tarefas/security/SecurityConfigurations.java
@@ -24,7 +24,7 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
