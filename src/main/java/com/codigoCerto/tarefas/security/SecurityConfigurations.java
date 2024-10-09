a236afef08bacf9e72fbc2dcdc1221effb09a5d9
Alan Santiago <alanorsantiago10@gmail.com>
2024-10-08 21:50:02 -0300

    feat: update SecurityConfigurations

--- src/main/java/com/codigoCerto/tarefas/security/SecurityConfigurations.java
+++ src/main/java/com/codigoCerto/tarefas/security/SecurityConfigurations.java
@@ -1,5 +1,8 @@
 package com.codigoCerto.tarefas.security;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.apache.catalina.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +19,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@SecurityScheme(name = SecurityConfigurations.SECURITY, type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class SecurityConfigurations {
+    public static final String SECURITY = "bearerAuth";
 
     @Autowired
     SecurityFilter securityFilter;
@@ -32,6 +37,7 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST,"/tarefa").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT,"/tarefa/{id}").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE,"/tarefa/{id}").hasRole("ADMIN")
+                        .requestMatchers("swagger-ui/**","swagger-ui.html","/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
